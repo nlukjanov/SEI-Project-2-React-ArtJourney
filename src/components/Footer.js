91475d0d8164dd2e4e1d1a18ad210f6fb9a00334
Nikita Lukjanov <nlukjanov@gmail.com>
2020-01-23 18:21:55 +0000

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className='footer content has-text-centered footer-text'>
+    <a href="https://data.rijksmuseum.nl/object-metadata/api/" target='_blank' rel="noopener noreferrer">Made with Rijksmuseum API</a>
+  </footer>
+)
+
+export default Footer
