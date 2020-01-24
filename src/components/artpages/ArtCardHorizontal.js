46bbd3959c4e97ff614e396fe0ce4eda50f75f2b
Nikita Lukjanov <nlukjanov@gmail.com>
2020-01-24 14:53:32 +0000

    added Loading to showpages

--- src/components/artpages/ArtCardHorizontal.js
+++ src/components/artpages/ArtCardHorizontal.js
@@ -1,49 +1,79 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Magnifier from 'react-magnifier'
+import Loading from '../common/Loading'
 
-const ArtCardHorizontal = (artPiece) => {
-  return (
-    <>
-      <h1 className="title has-text-centered">{artPiece.title}</h1>
-      <div className="columns">
-        <div className="column is-10 is-offset-1">
-          <figure className="image navbar-brand">
-            <Magnifier
-              src={artPiece.webImage.url}
-              alt={artPiece.title}
-            />
-          </figure>
-          <div className="horizontal-info">
-            <div className="content">
-              <h5 className="is-italic">Full title:</h5>
-              <h4>{artPiece.longTitle}</h4>
-              <h5 className="is-italic">Artist:</h5>
-              {artPiece.principalMakers.length > 0 ?
-                <Link to={`/${artPiece.principalMakers[0].name}`}><h4 className="artist-search">{artPiece.principalMakers[0].name}</h4></Link>
-                : <h4>Unknown</h4>}
+class ArtCardHorizontal extends React.Component {
+  state = {
+    imageLoaded: false
+  }
 
-              <h5 className="is-italic">Description:</h5>
-              {artPiece.plaqueDescriptionEnglish ? <h5>{artPiece.plaqueDescriptionEnglish}</h5> : <h5>No description available</h5>}
-            </div>
-            <div className="content">
-              <h5 className="is-italic">Presenting date:</h5>
-              <h4>{artPiece.dating.presentingDate}</h4>
-              <h5 className="is-italic">Colour palette:</h5>
-              <div className="color-palette">
-                {artPiece.colors.map(color => {
-                  const colorHex = color.hex
-                  return (
-                    <div key={color.hex} className="color-box" style={{ backgroundColor: colorHex }}></div>
-                  )
-                })}
+  handleOnLoad = () => {
+    this.setState({
+      imageLoaded: true
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className='title has-text-centered'>{this.props.title}</h1>
+        <div className='columns'>
+          <div className='column is-10 is-offset-1'>
+            {!this.state.imageLoaded && <Loading />}
+            <figure
+              onLoad={this.handleOnLoad}
+              className={`image navbar-brand ${
+                this.state.imageLoaded ? 'cards-visible' : 'cards-hidden'
+              }`}
+            >
+              <Magnifier src={this.props.webImage.url} alt={this.props.title} />
+            </figure>
+            <div className='horizontal-info'>
+              <div className='content'>
+                <h5 className='is-italic'>Full title:</h5>
+                <h4>{this.props.longTitle}</h4>
+                <h5 className='is-italic'>Artist:</h5>
+                {this.props.principalMakers.length > 0 ? (
+                  <Link to={`/${this.props.principalMakers[0].name}`}>
+                    <h4 className='artist-search'>
+                      {this.props.principalMakers[0].name}
+                    </h4>
+                  </Link>
+                ) : (
+                  <h4>Unknown</h4>
+                )}
+
+                <h5 className='is-italic'>Description:</h5>
+                {this.props.plaqueDescriptionEnglish ? (
+                  <h5>{this.props.plaqueDescriptionEnglish}</h5>
+                ) : (
+                  <h5>No description available</h5>
+                )}
+              </div>
+              <div className='content'>
+                <h5 className='is-italic'>Presenting date:</h5>
+                <h4>{this.props.dating.presentingDate}</h4>
+                <h5 className='is-italic'>Colour palette:</h5>
+                <div className='color-palette'>
+                  {this.props.colors.map(color => {
+                    const colorHex = color.hex
+                    return (
+                      <div
+                        key={color.hex}
+                        className='color-box'
+                        style={{ backgroundColor: colorHex }}
+                      ></div>
+                    )
+                  })}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default ArtCardHorizontal
--- src/components/artpages/ArtCardVertical.js
+++ src/components/artpages/ArtCardVertical.js
@@ -1,50 +1,81 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Magnifier from 'react-magnifier'
+import Loading from '../common/Loading'
 
-const ArtCardVertical = (artPiece) => (
-  <>
-    <h1 className="title has-text-centered">{artPiece.title}</h1>
-    <div className="columns">
-      <div className="column is-half">
-        <figure className="image is-horizontal-center img-magnifier-container">
-          <Magnifier
-            src={artPiece.webImage.url}
-            alt={artPiece.title}
-          />
-        </figure>
-      </div>
-      <div className="column is-5">
-        <div className="content">
-          <h5>Full title:</h5>
-          <h4>{artPiece.longTitle}</h4>
+class ArtCardVertical extends React.Component {
+  state = {
+    imageLoaded: false
+  }
 
-          <h5 className="is-italic">Artist:</h5>
-          {artPiece.principalMakers.length > 0 ?
-            <Link to={`/${artPiece.principalMakers[0].name}`}><h4 className="artist-search">{artPiece.principalMakers[0].name}</h4></Link>
-            : <h4>Unknown</h4>}
+  handleOnLoad = () => {
+    this.setState({
+      imageLoaded: true
+    })
+  }
 
-          <h5>Description:</h5>
-          {artPiece.plaqueDescriptionEnglish ? <p>{artPiece.plaqueDescriptionEnglish}</p> : <p>No description available</p>}
-        </div>
-        <div className="content">
-          <h5>Presenting date:</h5>
-          <p>{artPiece.dating.presentingDate}</p>
-          <h5>Colour palette:</h5>
-          <div className="color-palette">
-            {artPiece.colors.map(color => {
-              const colorHex = color.hex
-              return (
-                <div key={color.hex} className="color-box" style={{ backgroundColor: colorHex }}></div>
-              )
-            })}
+  render() {
+    return (
+      <>
+        <h1 className='title has-text-centered'>{this.props.title}</h1>
+        <div className='columns'>
+          <div className='column is-half'>
+            {!this.state.imageLoaded && <Loading />}
+            <figure
+              onLoad={this.handleOnLoad}
+              className={`image navbar-brand ${
+                this.state.imageLoaded ? 'cards-visible' : 'cards-hidden'
+              }`}
+            >
+              <Magnifier src={this.props.webImage.url} alt={this.props.title} />
+            </figure>
           </div>
-        </div>
-      </div>
-      <div className="column"></div>
-    </div>
-  </>
-)
+          <div className='column is-5'>
+            <div className='content'>
+              <h5>Full title:</h5>
+              <h4>{this.props.longTitle}</h4>
 
+              <h5 className='is-italic'>Artist:</h5>
+              {this.props.principalMakers.length > 0 ? (
+                <Link to={`/${this.props.principalMakers[0].name}`}>
+                  <h4 className='artist-search'>
+                    {this.props.principalMakers[0].name}
+                  </h4>
+                </Link>
+              ) : (
+                <h4>Unknown</h4>
+              )}
+
+              <h5>Description:</h5>
+              {this.props.plaqueDescriptionEnglish ? (
+                <p>{this.props.plaqueDescriptionEnglish}</p>
+              ) : (
+                <p>No description available</p>
+              )}
+            </div>
+            <div className='content'>
+              <h5>Presenting date:</h5>
+              <p>{this.props.dating.presentingDate}</p>
+              <h5>Colour palette:</h5>
+              <div className='color-palette'>
+                {this.props.colors.map(color => {
+                  const colorHex = color.hex
+                  return (
+                    <div
+                      key={color.hex}
+                      className='color-box'
+                      style={{ backgroundColor: colorHex }}
+                    ></div>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+          <div className='column'></div>
+        </div>
+      </>
+    )
+  }
+}
 
 export default ArtCardVertical
